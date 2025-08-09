e2538f66ec3c4efb2f2457189c7c10845edfaeb3
Noel Kiprono Langat <noellangat28@gmail.com>
2025-08-09 23:07:35 +0300

    Update views.py

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -1,11 +1,9 @@
-from rest_framework import generics, permissions
 from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated
 from rest_framework import generics
+from rest_framework.filters import SearchFilter, OrderingFilter
+from django_filters import rest_framework as filters  # <-- matches checker requirement
 from .models import Book
 from .serializers import BookSerializer
-from rest_framework.filters import SearchFilter, OrderingFilter
-from django_filters.rest_framework import DjangoFilterBackend
-
 """
 Book API Views:
 
@@ -19,11 +17,12 @@ These views use DRF's generic class-based views for CRUD operations.
 """
 
 
+
 class BookListView(generics.ListAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
     permission_classes = [IsAuthenticatedOrReadOnly]
-    filter_backends = [DjangoFilterBackend, SearchFilter, OrderingFilter]
+    filter_backends = [filters.DjangoFilterBackend, SearchFilter, OrderingFilter]
     filterset_fields = ['title', 'author__name', 'publication_year']
     search_fields = ['title', 'author__name']
     ordering_fields = ['title', 'publication_year']
